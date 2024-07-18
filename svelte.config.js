752cc7a030d7d15b032cef09ede1d10da2f9361b
Tom Buckley <tbuckley89@gmail.com>
2024-07-17 22:45:51 -0400

    fix: change svelte config to avoid breakage with __playwright dev-only route

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		prerender: {
+			handleHttpError: ({path, message}) => {
+				if(path === "/__playwright") {
+					return;
+				}
+				throw new Error(message);
+			}
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
