620cf75ee9b08aa926232e15422230841f4184ca
Tom Buckley <tbuckley89@gmail.com>
2024-07-17 22:42:10 -0400

    fix: unbreak cloudflare build

--- src/routes/__playwright/+layout.server.ts
+++ src/routes/__playwright/+layout.server.ts
@@ -1,10 +1,10 @@
 import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
-import { PLAYWRIGHT } from '$env/static/private';
+import * as staticEnv from '$env/static/private';
 import { dev } from '$app/environment';
 
 export const load: LayoutServerLoad = () => {
-	if (!dev && !PLAYWRIGHT) {
+	if (!dev && !staticEnv.PLAYWRIGHT) {
 		error(404, 'Not found');
 	}
 
