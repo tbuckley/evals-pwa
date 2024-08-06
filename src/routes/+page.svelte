d4afc86b37ada8356008874ea4f08c30e7e12eb5
Tom Buckley <tbuckley89@gmail.com>
2024-08-06 17:02:47 -0400

    docs: Improve the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,13 +23,46 @@
 
 <article class="prose">
 	{#if $configStore === null}
+		<h1>Welcome to Evals PWA!</h1>
 		<p>
-			Welcome to Evals PWA! Your can evaluate popular LLM models using your own prompts and test
-			cases, all from within your browser. Apart from requests directly to the LLM providers (like
-			OpenAI or Google), no data is sent to every server. Your configuration is stored on your file
-			system, and API keys are saved to your browser's local storage.
+			You can evaluate popular LLM models using your own prompts and test cases, all from within
+			your browser. Apart from requests directly to the LLM providers (like OpenAI or Google), no
+			data ever leaves your device. Your configuration is stored on your file system, and API keys
+			are saved to your browser's local storage.
 		</p>
-		<a href="##" on:click|preventDefault={chooseFolder}>Choose a folder</a> to get started
+		<h2>Getting started</h2>
+		<p>
+			To get started, <a href="##" on:click|preventDefault={chooseFolder}>choose a folder</a>
+			containing a <code>config.yaml</code> file that looks like this:
+		</p>
+		<pre>
+# An optional short description to help identify outputs
+description: My first eval
+
+# One or more prompts you are testing
+prompts:
+	- Respond like a pirate to this request: {'{{request}}'}}
+	- Respond with a haiku to this request: {'{{request}}'}}
+
+# One or more providers you want to use for running the prompts
+providers:
+  - gemini:gemini-1.5-pro-latest
+  - openai:gpt-4o
+
+# Tests provide values to feed into the prompts, and
+# checks to make sure the output is right
+tests:
+	- description: foo
+		vars:
+			request: Who was the first US president?
+		assert:
+			- type: contains
+				vars:
+					needle: washington
+</pre>
+		<p>
+			For more details, check out the <a href="/documentation">documentation</a>.
+		</p>
 	{:else}
 		<Button on:click={runTests}>Run tests</Button>
 	{/if}
