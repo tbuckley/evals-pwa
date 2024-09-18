7315abbf03d4f35a4ff67dffcdfabdc2ff086834
dstoc <539597+dstoc@users.noreply.github.com>
2024-09-18 11:31:26 +1000

    feat: Cache and reuse the web-llm model across tests

--- src/lib/providers/web-llm.ts
+++ src/lib/providers/web-llm.ts
@@ -3,6 +3,7 @@ import {
   type InitProgressReport,
   type ChatCompletionMessageParam,
   type CompletionUsage,
+  MLCEngine,
 } from '@mlc-ai/web-llm';
 import type { ModelProvider, ModelUpdate, MultiPartPrompt, TokenUsage } from '$lib/types';
 import { cast } from '$lib/utils/asserts';
@@ -12,55 +13,76 @@ interface Response {
   usage?: CompletionUsage;
 }
 
+const cache = new Map<
+  string,
+  {
+    refCount: number;
+    load: Promise<MLCEngine>;
+  }
+>();
+
 export class WebLlm implements ModelProvider {
   constructor(public model: string) {}
 
   async *run(prompt: MultiPartPrompt) {
     const progress = generator<InitProgressReport>();
-    const load = CreateMLCEngine(
-      this.model,
-      // TODO: allow providers to yield status in addition to deltas
-      {
-        initProgressCallback: (report) => {
-          progress.yield(report);
-        },
-      },
-    ).then((engine) => {
-      progress.return();
-      return engine;
-    });
-    for await (const report of progress.generator) {
-      yield { type: 'replace', output: report.text } as ModelUpdate;
-    }
-    yield { type: 'replace', output: '' } as ModelUpdate;
-    const engine = await load;
-    const input = prompt.map((part) => ('text' in part ? part.text : '')).join('\n');
-    const messages: ChatCompletionMessageParam[] = [
-      // { role: 'system', content: 'You are a helpful AI assistant.' },
-      { role: 'user', content: input },
-    ];
+    let cacheEntry;
+    let engine;
+    try {
+      if (!cache.has(this.model)) {
+        const load = CreateMLCEngine(this.model, {
+          initProgressCallback: (report) => {
+            progress.yield(report);
+          },
+        }).then((engine) => {
+          progress.return();
+          return engine;
+        });
+        cacheEntry = { refCount: 1, load };
+        cache.set(this.model, cacheEntry);
+        for await (const report of progress.generator) {
+          yield { type: 'replace', output: report.text } as ModelUpdate;
+        }
+        yield { type: 'replace', output: '' } as ModelUpdate;
+      } else {
+        cacheEntry = cast(cache.get(this.model));
+        cacheEntry.refCount++;
+      }
+      engine = await cacheEntry.load;
+      const input = prompt.map((part) => ('text' in part ? part.text : '')).join('\n');
+      const messages: ChatCompletionMessageParam[] = [
+        // { role: 'system', content: 'You are a helpful AI assistant.' },
+        { role: 'user', content: input },
+      ];
 
-    const chunks = await engine.chat.completions.create({
-      messages,
-      stream: true,
-      stream_options: { include_usage: true },
-    });
-    let reply = '';
-    let usage: CompletionUsage | undefined;
-    for await (const chunk of chunks) {
-      const delta = chunk.choices[0]?.delta.content ?? '';
-      yield delta;
-      reply += delta;
-      if (chunk.usage) {
-        usage = chunk.usage;
+      const chunks = await engine.chat.completions.create({
+        messages,
+        stream: true,
+        stream_options: { include_usage: true },
+      });
+      let reply = '';
+      let usage: CompletionUsage | undefined;
+      for await (const chunk of chunks) {
+        const delta = chunk.choices[0]?.delta.content ?? '';
+        yield delta;
+        reply += delta;
+        if (chunk.usage) {
+          usage = chunk.usage;
+        }
+      }
+      return {
+        reply,
+        usage,
+      } as Response;
+    } finally {
+      if (cacheEntry) {
+        cacheEntry.refCount--;
+        if (cacheEntry.refCount === 0) {
+          cache.delete(this.model);
+          await engine?.unload();
+        }
       }
     }
-    // TODO: Can we cache/reuse the engine across prompts somehow?
-    await engine.unload();
-    return {
-      reply,
-      usage,
-    } as Response;
   }
 
   extractOutput(response: unknown): string {
